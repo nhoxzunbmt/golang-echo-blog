b947fc5807f1c13e0f65c254d82f94b8483af45b
Loi tran <loi.tran@fitin.vn>
2020-06-14 19:52:38 +0700

    u

--- main.go
+++ main.go
@@ -47,5 +47,5 @@ func main() {
 	as := store.NewArticleStore(d)
 	h := handler.NewHandler(us, as)
 	h.Register(v1)
-	r.Logger.Fatal(r.Start("127.0.0.1:8585"))
+	r.Logger.Fatal(r.Start("127.0.0.1:9001"))
 }
